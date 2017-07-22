eea18618203ebd15209325916e359698a5b449eb
Vanessa Milagros <vanessapr.85@gmail.com>
2017-07-22 11:54:53 -0500

    added menu with react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
 import Topbar from './Topbar';
 import TopArtistsContainer from './artists/TopArtistsContainer';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Topbar />
-        <div className="grid-x grid-padding-x">
-          <div className="cell medium-4 medium-cell-block-y">
-            User
-          </div>
-          <div className="cell medium-8 medium-cell-block-y">
-            <TopArtistsContainer />
+      <Router>
+        <div>
+          <Topbar />
+          <div className="grid-x grid-padding-x">
+            <div className="cell medium-4 medium-cell-block-y">
+              <ul className="vertical menu">
+                <li><NavLink exact to="/">Top Artists</NavLink></li>
+                <li><NavLink to="/">Your favorite artists</NavLink></li>
+              </ul>
+            </div>
+            <div className="cell medium-8 medium-cell-block-y">
+              <Switch>
+                <Route path="/" component={TopArtistsContainer} />
+              </Switch>
+            </div>
           </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
