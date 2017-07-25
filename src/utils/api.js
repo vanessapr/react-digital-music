a9eab7dd8758fd09453c4af7dfcdc7ad326fd1c8
Vanessa Milagros <vanessapr.85@gmail.com>
2017-07-24 21:28:29 -0500

    change http by https for api

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import config from '../config';
 
 function getTopArtists(country = 'peru') {
-  return axios.get(`http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=${config.LASTFM_API_KEY}&format=json`)
+  return axios.get(`https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=${config.LASTFM_API_KEY}&format=json`)
     .then(response => response.data.topartists);
 }
 
