2140ee9840ec8508548e479725ea0d5158600fa2
Vanessa Milagros <vanessapr.85@gmail.com>
2017-07-29 12:52:37 -0500

    added styles for profile

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,13 +3,8 @@ import { NavLink } from 'react-router-dom';
 
 const Navigation = () => (
   <div>
-    <h3 className="separator-center">Menu</h3>
+    <h3 className="separator-right">Menu</h3>
     <ul className="vertical menu align-right">
-      <li>
-        <NavLink to="/profile">
-          <i className="zmdi zmdi-edit zmdi-hc-lg"></i> Edit profile
-        </NavLink>
-      </li>
       <li>
         <NavLink exact to="/">
         <i className="zmdi zmdi-equalizer zmdi-hc-lg"></i> Top Artists
--- src/components/Users/Profile.js
+++ src/components/Users/Profile.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import avatar from './profile.jpg';
-import './profile.css';
+import './Profile.css';
 
 const Profile = ({ user, isLoading, isLoggedIn }) => {
   return (
       isLoggedIn &&
-      <div className="__profile">
-        <div className="__profile_img">
+      <div className="profile">
+        <div className="profile-img">
           <img src={avatar} alt={user.email} width="200" />
         </div>
-        <div className="__profile_info">
-          <p>{ user.displayName }</p>
+        <div className="profile-info text-center">
+          <h6><Link to="/profile">{ user.displayName }</Link></h6>
+          <p>{ user.email }</p>
         </div>
       </div>
     )
